118f63923fd13801ecafb102f30fc0ed4e3aa1d4
M Gopal Krishna <mgopalkrishna2001@gmail.com>
2023-01-29 13:56:36 +0530

    completed Assignment-Java_Fetch_API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,10 @@
-package org.example;public class Main {
+package org.example;
+import org.json.JSONObject;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+public class Main {
     public static void main(String[] args) {
         try{
             FetchDataUsingJava_1();
@@ -6,8 +12,7 @@ package org.example;public class Main {
             e.printStackTrace();
         }
     }
-
-    public static void FetchDataUsingJava_1() throws IOException, InterruptedException {
+    public static void FetchDataUsingJava_1() throws Exception {
         String URL = "https://api.chucknorris.io/jokes/random";
         //building a request .
         HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(URL)).build();
@@ -16,15 +21,15 @@ package org.example;public class Main {
 
         HttpResponse<String> httpResponse = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
         System.out.println(httpResponse.body());
-//        System.out.println(httpResponse.statusCode());
-
+        System.out.println(httpResponse.statusCode());
         JSONObject jsonObject = new JSONObject(httpResponse.body());
+        System.out.println("This is a json Response");
         System.out.println(jsonObject);
         String updated = jsonObject.getString("updated_at");
-        System.out.println(updated);
+        System.out.println("This is updated time:"+updated);
         String Created = jsonObject.getString("created_at");
-        System.out.println(Created);
+        System.out.println("This is Created time:"+Created);
         String Value = jsonObject.getString("value");
-        System.out.println(Value);
+        System.out.println("This is the value:"+Value);
     }
 }
